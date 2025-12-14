defcced0059647c937e13ba7a39338ad41656cdb
Likith M R <likithchengappa421@gmail.com>
2025-12-14 07:45:26 +0530

    Add SQL queries to retrieve occupation data

---
+++ Occupation.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(Name,'(',UPPER(LEFT(Occupation,1)),')')
+FROM OCCUPATIONS
+ORDER BY Name;
+
+SELECT CONCAT('There are a total of ',COUNT(*),' ',LOWER(Occupation),'s.')
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(*), Occupation;
